avgpool2d + classification + bug on backprop

conv_test.py
@@ -4,8 +4,11 @@ import numpy as np
 import matplotlib.pyplot as plt
 from PIL import Image
 
+import nn
 from tensor import Tensor
+from loss import BCELoss
 
+classes = {0: "not-cat", 1: "cat"}
 
 if __name__ == "__main__":
   img = Image.open("./media/cat.jpeg")
@@ -18,18 +21,38 @@ if __name__ == "__main__":
   t_in = Tensor(img, name="t_in")
   print("t_in:", t_in)
   gt = Tensor(np.ones((1,1)), name="ground_truth")  # for binary classification
+  print("Ground Truth:", gt)
+  print("=>", classes[gt.data[0][0]])
 
   conv_time = time()
-  t_out = t_in.conv2d(3, 6, 5)
-  print("t_out:", t_out)
+  t1 = t_in.conv2d(3, 6, 5)
+  print("t1:", t1)
   print("Time elapsed for Conv2D op: %.2f sec"%(time() - conv_time))
+  conv_img = t1.data
 
-  pool_time = time()
-  t_out = t_out.maxpool2d()
-  print("t_out:", t_out)
-  print("Time elapsed for MaxPool2D op: %.2f sec"%(time() - pool_time))
+  # pool_time = time()
+  # t2 = t2.maxpool2d()
+  # print("t2:", t2)
+  # print("Time elapsed for MaxPool2D op: %.2f sec"%(time() - pool_time))
 
-  # TODO: display images in grid
-  out_img = Image.fromarray(t_out.data)
-  plt.imshow(out_img, cmap="gray")
-  plt.show()
+  # TODO: add ReLU
+
+  t1.flatten()
+  fc = nn.Linear(t1.data.shape[0], 1)
+  t1.layer = fc
+  t2 = fc(t1)
+  print("t2:", t2)
+  print(t2.data)
+
+  t3 = t2.softmax()
+  print("t3:", t3)
+  print(t3.data)
+
+  loss = BCELoss(t3, gt) # for binary classification
+  print("Loss:", loss.data)
+  loss.backward()
+
+  # TODO: display images/feature-maps in grid
+  # out_img = Image.fromarray(conv_img)
+  # plt.imshow(out_img, cmap="gray")
+  # plt.show()

tensor.py
@@ -12,7 +12,8 @@ OPS = {"Linear": 0,
        "MAELoss": 7,
        "CrossEntropyLoss": 8,
        "BCELoss": 9,
-       "MaxPool2D": 10
+       "MaxPool2D": 10,
+       "AvgPoll2D": 11
       }
 
 def get_key_from_value(d, val):
@@ -131,6 +132,9 @@ class Tensor:
   def mean(self):
     return np.mean(self.data)
 
+  def flatten(self):
+    self.data = self.data.flatten()
+
   # pretty print the graph for this tensor backwards
   def print_graph(self, verbose=False):
     tmp = list(reversed(list(self._prev.copy())))
@@ -180,7 +184,7 @@ class Tensor:
     assert len(self.data.shape) == 2, "Conv2D input tensor must be 2D"  # TODO: 3D tensors (RGB)
     assert kernel_size in [3, 5, 7], "Conv2D kenrel_size must be 3, 5, or 7"
 
-    self.kernel = np.random.randint(0, 255, (kernel_size**2,), dtype=np.uint8)  # weight
+    self.kernel = Tensor(np.random.randint(0, 255, (kernel_size**2,), dtype=np.uint8), "conv_kernel")  # weight
     self.b = bias   # TODO: bias is an array of image size (W * H)
     self.out = Tensor(np.ones_like(self.data), "conv2d_out", _children=self._prev.copy())
     self.out._prev.append(self)
@@ -206,13 +210,15 @@ class Tensor:
           for l in range(j-bdr, j+bdr-1, 1):
             if j - bdr < 0 or j + bdr >= self.data.shape[1]:
               continue
-            summ += self.data[k][l] * self.kernel[kernel_idx]
+            summ += self.data[k][l] * self.kernel.data[kernel_idx]
             kernel_idx += 1
         self.out.data[i][j] = summ
 
-    # TODO: write this
+    # TODO: this is not good code, yet
     def _backward():
-      pass
+      self.kernel.grad = np.dot(self.data.T, self.out.grad)
+      #self.b.grad = np.sum(self.out.grad, axis=0, keepdims=True)
+      self.grad = np.dot(self.out.grad, self.kernel.data.T)
     self.out._backward = _backward
 
     return self.out
@@ -249,8 +255,31 @@ class Tensor:
 
     return self.out
 
-  def avgpool(self):
-    pass
+  def avgpool(self, filter=(2,2), stride=1, padding=0):
+    # TODO: assert dimensionality
+    # TODO: handle channels and padding as well
+    # TODO: double-check if stride is used correctly
+
+    # pooling out_dim = (((input_dim - filter_dim) / stride) + 1) * channels
+    out_img = np.ones(((self.data.shape[0] - filter[0] // stride) + 1, (self.data.shape[1] - filter[1] // stride) + 1))
+    for i in range(0, self.data.shape[0]-filter[0], filter[0]):
+      for j in range(0, self.data.shape[1]-filter[0], filter[1]):
+        tmp = []
+        for n in range(filter[0]):
+          for m in range(filter[1]):
+            # TODO: keep pooling (max) indices, for use on GANs (like SegNet)
+            tmp.append(out_img[i*stride+n][j*stride+m])
+        out_img[i][j] = np.array(tmp).mean()
+
+    self.out = Tensor(out_img, "avgpool2d", _children=self._prev.copy())
+    self.out._prev.append(self)
+    self.out.prev_op = OPS["AvgPool2D"]
+
+    def _backward():
+      self.grad = self.out.grad
+    self.out._backward = _backward
+
+    return self.out
 
   # TODO: backward needs to implemented for all tensors in each op (a = b + c => a.back -> b.back and c.back)
   # that's why deepwalk is implemented