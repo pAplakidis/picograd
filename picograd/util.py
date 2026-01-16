074550c98ce90319cb0d1f616c56b2dbc5be8afc
pAplakidis <aplakidisp@gmail.com>
2026-01-16 22:11:53 +0200

    deterministic order of tensor._prev + fix shape&strides for non-contiguous tensors + tensor.contiguous() + tensor.sum() + movement ops

--- picograd/util.py
+++ picograd/util.py
@@ -1,4 +1,5 @@
 import subprocess
+from typing import Tuple
 
 MAX_GRAD_NORM = 1.0
 
@@ -11,7 +12,7 @@ def is_cuda_available() -> bool:
   except (subprocess.CalledProcessError, FileNotFoundError):
     return False
 
-def default_strides(shape: tuple) -> tuple:
+def default_strides(shape: Tuple) -> Tuple:
   """ Get numpy-style contiguous row-major strides from shape (for float32 = 4 bytes) """
   stride = 1
   strides = []
@@ -20,5 +21,18 @@ def default_strides(shape: tuple) -> tuple:
     stride *= dim
   return tuple(reversed(strides))
 
-def check_contiguous(shape: tuple, strides: tuple) -> bool:
+def check_contiguous(shape: Tuple, strides: Tuple) -> bool:
   return strides == default_strides(shape)
+
+def get_real_shape(shape: Tuple, strides: Tuple) -> Tuple[int]:
+  """ Get the actual shape of a tensor given its shape and strides, accounting for broadcasted dimensions """
+  return tuple(dim for dim, stride in zip(shape, strides) if stride != 0)
+
+def required_numel(shape: Tuple, strides: Tuple):
+  """ Compute how much backing memory is required for a tensor with given shape and strides """
+  max_offset = 0
+  for dim, stride in zip(shape, strides):
+      if dim == 0:
+          return 0
+      max_offset += (dim - 1) * stride
+  return max_offset + 1
